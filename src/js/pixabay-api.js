4a1d4d964937701636b3e64937ba32a51ff38ada
Yurii Ivanes <ivanecura82@gmail.com>
2026-05-13 02:24:38 +0300

    fix05

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
+const API_KEY = '55836567-c9271527a299f964d26049cdd';
+
 export const searchImage = async (query, page, per_page) => {
+  const safePerPage = Math.max(per_page, 15);
+
   const response = await axios.get('https://pixabay.com/api/', {
     params: {
-      key: 'YOUR_API_KEY',
+      key: API_KEY,
       q: query,
       page,
-      per_page,
+      per_page: safePerPage,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
